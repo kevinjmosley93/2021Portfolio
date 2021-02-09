487350d087eb96fd06042cd9c088b0c22da6503d
kevinjmosley93 <kevinjmosley93@gmail.com>
2021-02-08 22:59:33 -0600

    Latest Commit

--- components/Content.js
+++ components/Content.js
@@ -2,18 +2,29 @@ import { Col, Container, Image, Jumbotron, Row, Button } from 'react-bootstrap'
 
 export default function Hero() {
   return (
-    <Container>
-      <h1>Hey, I'm Kevin!</h1>
-      <p>
-        After 5 years in the U.S. Navy, I now build modern Web Applications with
-        mostly <span className='bg-dark text-info'>React</span> &{' '}
-        <span className='bg-dark text-info'>Nodejs</span>
-      </p>
-      <p>
-        <Button className='' variant='outline-dark'>
-          Learn more
-        </Button>
-      </p>
+    <Container className='mt-4'>
+      <Row>
+        <Col sm={12} lg={8}>
+          <h1>Hey, I'm Kevin!</h1>
+          <p>
+            After 5 years in the U.S. Navy, I now build modern Web Applications
+            with mostly <span className='bg-dark text-info'>React</span> &{' '}
+            <span className='bg-dark text-info'>Nodejs</span>
+          </p>
+          <p>
+            <Button className='' variant='outline-dark'>
+              Learn more
+            </Button>
+          </p>
+        </Col>
+        <Col sm={12} lg={4}>
+          <Image
+            height='300px'
+            width='300px'
+            src='https://i.imgur.com/GDsN4LQ.jpg'
+          />
+        </Col>
+      </Row>
     </Container>
   )
 }
