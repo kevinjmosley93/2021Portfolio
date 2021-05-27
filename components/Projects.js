c5be23290be648a2f0ca3d9c295571f1277008b7
kevinjmosley93 <kevinjmosley93@gmail.com>
2021-05-26 22:35:47 -0500

    Change

--- components/Projects.js
+++ components/Projects.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ProjectCard } from 'components/ProjectCard'
-import { projects } from 'components/data/data'
 import { Col, Container, Row } from 'react-bootstrap'
 
-const Projects = ({ data }) => {
+const Projects = () => {
+  const [data, setData] = useState([])
+  const fetchData = async () => {
+    const res = await fetch(`${window.location.href}api/get-projects`)
+    const data = await res.json()
+    console.log('this is data', data)
+    setData(data)
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
   return (
     <Container>
       <h1 className='text-center'>Projects</h1>
       <Row>
-        {data.map(pro => {
-          console.log('this is skill:', pro)
-          return <ProjectCard key={pro.id} project={pro} />
-        })}
+        {data.length > 0 &&
+          data.map(pro => {
+            console.log('this is skill:', pro)
+            return <ProjectCard key={pro.id} project={pro} />
+          })}
       </Row>
     </Container>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,13 @@
-import { useEffect } from 'react'
 import Content from '../components/Content'
 import Header from '../components/Header'
 import Projects from '../components/Projects'
 
-export default function Home({ data }) {
-  useEffect(() => {
-    console.log('this is data', data)
-  }, [])
+export default function Home() {
   return (
     <div className=''>
       <Header />
       <Content />
-      <Projects data={data} />
+      <Projects />
     </div>
   )
-}
-
-export async function getServerSideProps(context) {
-  console.log('this is context', context.req.headers.referer)
-  const res = await fetch(`${window.location.hostname}/api/get-projects`)
-  const data = await res.json()
-
-  return {
-    props: { data } // will be passed to the page component as props
-  }
 }
