25fa8af3092e273596cdbc5164d779a5e9ecfc5d
kevinjmosley93 <kevinjmosley93@gmail.com>
2021-05-29 20:22:34 -0500

    Commit

--- components/Projects.js
+++ components/Projects.js
@@ -3,7 +3,7 @@ import { ProjectCard } from 'components/ProjectCard'
 import { Col, Container, Row } from 'react-bootstrap'
 
 const Projects = () => {
-  const [data, setData] = useState([])
+  const [projectData, setData] = useState([])
   const fetchData = async () => {
     const res = await fetch(`${window.location.href}api/get-projects`)
     const data = await res.json()
@@ -17,8 +17,8 @@ const Projects = () => {
     <Container>
       <h1 className='text-center'>Projects</h1>
       <Row>
-        {data.length > 0 &&
-          data.map(pro => {
+        {projectData.length > 0 &&
+          projectData.map(pro => {
             console.log('this is skill:', pro)
             return <ProjectCard key={pro.id} project={pro} />
           })}
